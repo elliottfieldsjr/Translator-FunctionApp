8670fddc130f3a5569501904709167138c82ec77
efields@mcitpnetworks.com <efields@mcitpnetworks.com>
2023-11-09 07:05:12 -0500

    Initial Upload

---
+++ dtfunctionsimple/__init__.py
@@ -0,0 +1,37 @@
+import logging
+import os
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.translation.document import DocumentTranslationClient
+import azure.functions as func
+
+
+def main(myblob: func.InputStream):
+    logging.info(f"Python blob trigger function processed blob \n"
+                 f"Name: {myblob.name}\n"
+                 f"Blob Size: {myblob.length} bytes")
+    
+    target_language=myblob.name.split('/')[1]
+    source_blob_url = myblob.uri
+
+    endpoint = os.environ["translator_endpoint"]
+    key = os.environ["translator_key"]
+    target_blob_url = os.environ["target_blob_url"] 
+
+
+    client = DocumentTranslationClient(endpoint, AzureKeyCredential(key))
+    logging.info("Begin Translation for: " + myblob.name)
+
+    poller = client.begin_translation(source_blob_url, target_blob_url, target_language, storage_type="File")
+    logging.info(f"Created translation operation with ID: {poller.id}")
+    logging.info("Waiting until translation completes...")
+    result = poller.result()
+
+    for document in result:
+        logging.info(f"Document ID: {document.id}")
+        logging.info(f"Document status: {document.status}")
+        if document.status == "Succeeded":
+            logging.info(f"Source document location: {document.source_document_url}")
+            logging.info(f"Translated document location: {document.translated_document_url}")
+            logging.info(f"Translated to language: {document.translated_to}\n")
+        elif document.error:
+            logging.info(f"Error Code: {document.error.code}, Message: {document.error.message}\n")
